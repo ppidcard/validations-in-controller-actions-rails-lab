cf15a9529192198813af3339887ce36bf9821435
ppidcard <tvxqlikei@live.cn>
2021-09-12 11:51:28 +1000

    Done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,24 @@
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
+
   def show
+    @post = Post.find(params[:id])
   end
 
+
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
-    @post.update(post_params)
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end 
 
-    redirect_to post_path(@post)
   end
 
   private
